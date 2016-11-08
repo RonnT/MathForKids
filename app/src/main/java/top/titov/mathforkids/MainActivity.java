88bed11dd45f86b26eb8f85124183925f5d1fe22
Roman Titov <ron74@bk.ru>
2016-11-08 23:31:35 +0500

    some changes in logic, landscape orientation. need fix padding for 10

--- app/src/main/java/top/titov/mathforkids/MainActivity.java
+++ app/src/main/java/top/titov/mathforkids/MainActivity.java
@@ -14,15 +14,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private static final int MAX_NUMBER = 5;
-    private static final int ANSWER_COUNT = 3;
+    private static final int ANSWER_COUNT = 4;
 
-    public List<String> itemList = new ArrayList<>();
+    public List<String> variantList = new ArrayList<>();
     private GridView answerGridView;
     private ArrayAdapter<String> adapter;
-    private ArrayList<Integer> numberList = new ArrayList<>();
+    private ArrayList<Integer> numberLineList = new ArrayList<>();
 
     private Integer firstNum;
     private Integer secondNum;
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity{
         generateEquation();
         clearAndFillAnswerList();
 
-        adapter = new ArrayAdapter<>(this, R.layout.answer_item, itemList);
+        adapter = new ArrayAdapter<>(this, R.layout.answer_item, variantList);
         answerGridView.setAdapter(adapter);
         answerGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -58,46 +58,47 @@ public class MainActivity extends AppCompatActivity{
 
     private void generateEquation() {
         Random random = new Random(System.currentTimeMillis());
-        firstNum = random.nextInt(MAX_NUMBER);
-        secondNum = random.nextInt(MAX_NUMBER);
+        firstNum = random.nextInt(MAX_NUMBER + 1);
+        secondNum = random.nextInt(MAX_NUMBER + 1);
         firstNumTextView.setText(String.valueOf(firstNum));
         secondNumTextView.setText(String.valueOf(secondNum));
         result = firstNum + secondNum;
     }
 
     private void clearAndFillAnswerList() {
-        itemList.clear();
+        variantList.clear();
         boolean hasAnswer = false;
-        fillAnswerArray();
+        fillNumberLine();
         for (int i = 0; i < ANSWER_COUNT; i++) {
             Integer answer = getNextAnswer();
             if (answer.equals(result)) hasAnswer = true;
-            itemList.add(String.valueOf(getNextAnswer()));
+            variantList.add(String.valueOf(answer));
         }
-        if (!hasAnswer){
+        if (!hasAnswer) {
             Random random = new Random(System.currentTimeMillis());
-            itemList.set(random.nextInt(ANSWER_COUNT), String.valueOf(result));
+            variantList.set(random.nextInt(ANSWER_COUNT), String.valueOf(result));
         }
 
     }
 
-    private void fillAnswerArray() {
+    private void fillNumberLine() {
+        numberLineList.clear();
         for (int i = 0; i < MAX_NUMBER * 2 + 1; i++) {
-            numberList.add(i);
+            numberLineList.add(i);
         }
     }
 
     private Integer getNextAnswer() {
         Random random = new Random(System.currentTimeMillis());
-        int position = random.nextInt(numberList.size());
-        Integer answer = numberList.get(position);
-        numberList.remove(position);
-        numberList.trimToSize();
+        int position = random.nextInt(numberLineList.size());
+        Integer answer = numberLineList.get(position);
+        numberLineList.remove(position);
+        numberLineList.trimToSize();
         return answer;
     }
 
     public void onItemClick(View view, int position) {
-        Integer answer = Integer.parseInt(itemList.get(position));
+        Integer answer = Integer.parseInt(variantList.get(position));
         if (answer.equals(result)) {
             view.setBackgroundColor(ContextCompat.getColor(MainActivity.this, android.R.color.holo_green_light));
             handler.postDelayed(new Runnable() {
@@ -105,7 +106,7 @@ public class MainActivity extends AppCompatActivity{
                 public void run() {
                     generateEquation();
                     clearAndFillAnswerList();
-                    adapter = new ArrayAdapter<>(MainActivity.this, R.layout.answer_item, itemList);
+                    adapter = new ArrayAdapter<>(MainActivity.this, R.layout.answer_item, variantList);
                     answerGridView.setAdapter(adapter);
                 }
             }, 1000);
